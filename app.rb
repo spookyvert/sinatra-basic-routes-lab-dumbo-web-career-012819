e5f6b8c40e77f02ad32742c6dca9ae853be3a6eb
Shemar Dacosta <dshemar6@gmail.com>
2019-02-17 15:46:40 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    "Hello, World!"
+  end
+  get '/name' do
+    "My name is Shemar"
+  end
+  get '/hometown' do
+    "My hometown is The Bronx"
+  end
+  get '/favorite-song' do
+    "My favorite song is new house"
+  end
 end

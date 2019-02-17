fd23bd9106b8f7af253cc44ceaaf28df527a966d
Shemar Dacosta <dshemar6@gmail.com>
2019-02-17 15:42:39 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,6 +2,9 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   get '/' do
-  "Hello, World!"
-end
+    "Hello, World!"
+  end
+  get '/name' do
+    "My name is Shemar"
+  end
 end
